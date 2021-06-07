74ea3c22d813459930863a13111ca105f125b06c
ghYoon93 <gh.yoon93@gmail.com>
2021-06-07 21:00:13 +0900

    Initial commit

---
+++ nbbang-api/src/test/java/com/hwagok/nbbang/NbbangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwagok.nbbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbbangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
